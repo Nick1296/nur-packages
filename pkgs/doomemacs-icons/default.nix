1f9086681232296814162058ce945ff05e9d17df
Mattia Nicolella <mattianicolella@gmail.com>
2024-10-09 23:18:31 -0400

    feat: add doomemacs icons metadata

--- pkgs/doomemacs-icons/default.nix
+++ pkgs/doomemacs-icons/default.nix
@@ -1,14 +1,24 @@
-{ stdenv, fetchurl }:
-let
-  icon = fetchurl {
-    url = "https://github.com/jaidetree/doom-icon/tree/e90e93ff6c05615137a0a3694f4674ba83ff00ae";
-    sha256 = "";
-  };
-in
-stdenv.mkDerivation rec {
+{ lib,stdenvNoCC, fetchFromGitHub }:
+stdenvNoCC.mkDerivation rec {
   name = "doomemacs-icons-${version}";
   version = "2023-03-10";
-  src =icon;
-  installPhase = ''install -Dm644 ${icon}/{abject,cute,emacs}-doom/ $out/share/doomemacs-icons/
-  rm -rf $out/share/doomemacs-icons/*/src'';
+  src = fetchFromGitHub {
+    owner = "jaidetree";
+    repo = "doom-icon";
+    rev = "e90e93ff6c05615137a0a3694f4674ba83ff00ae";
+    hash = "sha256-TLUzL37X41PBRK9oVtKjZDQ2Laf82OMA9KWzTjGKmyY=";
+  };
+  dontUnpack = true;
+  installPhase = ''
+    mkdir -p $out/share/doomemacs-icons/{abject,cute,emacs}-doom
+    cp $src/abject-doom/doom.* $out/share/doomemacs-icons/abject-doom/
+    cp $src/cute-doom/doom.* $out/share/doomemacs-icons/cute-doom/
+    cp $src/emacs-doom/doom.* $out/share/doomemacs-icons/emacs-doom/
+    '';
+  meta = with lib; {
+    description = "A proposed doom Emacs icon";
+    homepage = "https://github.com/jaidetree/doom-icon";
+    license = licenses.cc0;
+    platforms = platforms.all;
+  };
 }
